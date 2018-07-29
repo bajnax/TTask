384e4f8f1c524feb268d9ac7f7bd92b05e0bed38
bajnax <mr.baynak@gmail.com>
2018-07-29 19:39:08 +0300

    Avatar is rounded, 'status' icon's color is associated with the status and positioned properly

--- app/src/main/java/com/example/ttask/UsersAdapter.kt
+++ app/src/main/java/com/example/ttask/UsersAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.ttask
 
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +11,7 @@ import kotlinx.android.synthetic.main.list_item.view.*
 class UsersAdapter(private val users: List<User>): RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val itemV = LayoutInflater.from(p0?.context).inflate(R.layout.list_item, p0, false)
+        val itemV = LayoutInflater.from(p0.context).inflate(R.layout.list_item, p0, false)
         return ViewHolder(itemV)
     }
 
@@ -23,11 +24,29 @@ class UsersAdapter(private val users: List<User>): RecyclerView.Adapter<UsersAda
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
         fun bindViews(user: User) {
-            Picasso.get().load(user.avatar).into(itemView.avatar)  //use Picasso lib to download images
+
+            Picasso.get().load(user.avatar).into(itemView.avatar)
+
             itemView.name.text = user.name
-            itemView.similarity.text = user.similarity.toString()
-            itemView.status.text = user.status
+            itemView.age.text = user.age.toString() + " лет, "
             itemView.unread_messages.text = user.unreadMessages.toString()
+
+            // TODO: modify according to requirements
+            itemView.last_seen.text = user.lastSeen
+
+            itemView.similarity.text = user.similarity.toString() + "%"
+            when {
+                user.similarity < 40 -> itemView.similarity.setTextColor(Color.RED)
+                user.similarity < 70 -> itemView.similarity.setTextColor(Color.YELLOW)
+                else -> itemView.similarity.setTextColor(Color.GREEN)
+            }
+
+            when {
+                user.status == "dnd" -> itemView.status.setColorFilter(Color.RED)
+                user.status == "away" -> itemView.status.setColorFilter(Color.YELLOW)
+                else -> itemView.status.setColorFilter(Color.GREEN)
+            }
+
         }
     }
 
