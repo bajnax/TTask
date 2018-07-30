aa5f49174ad81e813293e16c6c4d7ef816d9ba30
bajnax <mr.baynak@gmail.com>
2018-07-31 01:37:50 +0300

    Added item divider

--- app/src/main/java/com/example/ttask/MainActivity.kt
+++ app/src/main/java/com/example/ttask/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ttask
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         getUsers()
     }
 
@@ -30,13 +32,13 @@ class MainActivity : AppCompatActivity() {
         call.enqueue(object: Callback<List<User>> {
             override fun onResponse(call: Call<List<User>>, response: retrofit2.Response<List<User>>?) {
                 if (response != null) {
-                    var users: List<User> = response.body()!!
+                    val users: List<User> = response.body()!!
                     recyclerView.adapter = UsersAdapter(users)
                 }
             }
 
             override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                Log.e("MainActivity", t.toString());
+                Log.e("MainActivity", t.toString())
             }
         })
     }
