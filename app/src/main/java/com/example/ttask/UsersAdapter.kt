d1df0f32e63aec8b2615e6bffe43a89cf87dcf17
bajnax <mr.baynak@gmail.com>
2018-07-31 23:05:46 +0300

    Changed colors and date formatting

--- app/src/main/java/com/example/ttask/UsersAdapter.kt
+++ app/src/main/java/com/example/ttask/UsersAdapter.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.list_item.view.*
-import java.text.SimpleDateFormat
+import org.joda.time.DateTime
+import org.joda.time.Days
+import org.joda.time.format.DateTimeFormat
 
 class UsersAdapter(private val users: List<User>): RecyclerView.Adapter<UsersAdapter.ViewHolder>() {
 
@@ -31,17 +33,34 @@ class UsersAdapter(private val users: List<User>): RecyclerView.Adapter<UsersAda
 
             itemView.name.text = user.name
             itemView.age.text = user.age.toString() + " лет, "
-            itemView.unread_messages.text = user.unreadMessages.toString()
 
-            // TODO: modify according to requirements
-            itemView.last_seen.text = user.lastSeen
+            if(user.unreadMessages != 0)
+                itemView.unread_messages.text = user.unreadMessages.toString()
+            else {
+                itemView.unread_messages.visibility = View.INVISIBLE
+            }
+
+
+            val sDateFormatter = DateTimeFormat.forPattern("DD.MM.yyyy HH:mm")
+            val outputFormatter = DateTimeFormat.forPattern("HH:mm")
+            val today = DateTime()
+
+            val receivedDate = sDateFormatter.parseDateTime(user.lastSeen)
+
+            when {
+                Days.daysBetween(receivedDate, today).days == 0 -> itemView.last_seen.text = "Сегодня, " + outputFormatter.print(receivedDate)
+                Days.daysBetween(receivedDate, today).days == 1 -> itemView.last_seen.text = "Вчера, " + outputFormatter.print(receivedDate)
+                else -> itemView.last_seen.text = user.lastSeen
+            }
+
 
             itemView.similarity.text = user.similarity.toString() + "%"
             when {
                 user.similarity < 40 -> itemView.similarity.setTextColor(Color.RED)
-                user.similarity < 70 -> itemView.similarity.setTextColor(Color.YELLOW)
+                user.similarity < 70 -> itemView.similarity.setTextColor(Color.parseColor("#ffcc80"))
                 else -> itemView.similarity.setTextColor(Color.GREEN)
             }
+
 
             when {
                 user.status == "dnd" -> itemView.status.setColorFilter(Color.RED)
